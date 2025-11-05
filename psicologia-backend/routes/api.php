ba93a61c21722ed468045511bd630de713d236f4
josue18K <josuern5593@gmail.com>
2025-11-05 09:46:13 -0500

    Cree AuthController y UserController

--- psicologia-backend/routes/api.php
+++ psicologia-backend/routes/api.php
@@ -3,4 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/prueba', function () {return "Api funcionando correctamente";});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
