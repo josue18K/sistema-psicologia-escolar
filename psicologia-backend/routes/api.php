f30dfba7d1fe2c786b7cd75100d31a5048c3f70c
Camilaipanaque <ipanaque258@gmail.com>
2025-11-07 13:00:00 -0500

    cree las rutas del AUTH controller

--- psicologia-backend/routes/api.php
+++ psicologia-backend/routes/api.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//RUTAS AUTH
+
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('logout',[AuthController::class, 'logout']);
+Route::get('/me',[AuthController::class,'me']);
+Route::post('/register',[Authcontroller::class, 'register']);
