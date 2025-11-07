7697ce5e479c65ee8e14bf2468e7a7199fc346de
JosePecho <pechojose22@gmail.com>
2025-11-07 13:00:11 -0500

    cree las rutas estudiantes

--- psicologia-backend/routes/api.php
+++ psicologia-backend/routes/api.php
@@ -6,3 +6,55 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ESTUDIANTE
+
+Route::get('/estudiantes',[EstudianteController::class,'index']);
+Route::get('/estudiantes',[EstudianteController::class,'store']);
+Route::get('/estudiantes/{dni}', [EstudianteController::class, 'show']);
+Route::put('/estudiantes/{dni}', [EstudianteController::class, 'update']);
+Route::delete('/estudiantes/{dni}', [EstudianteController::class, 'destroy']);
+Route::get('/estudiante-search',[EstudianteController::class, 'search']);
