777bc605a14e2514440958086f120c67c93ea12d
rosario142 <rosarioatanacioheydy02@gmail.com>
2025-11-07 12:08:59 -0600

    cree ruta de Usuarios

--- psicologia-backend/routes/api.php
+++ psicologia-backend/routes/api.php
@@ -6,3 +6,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// USUARIOS
+Route::get('/users', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
+Route::patch('/users/{user}/toggle-estado', [UserController::class, 'toggleEstado']);
+Route::get('/tutores', [UserController::class, 'tutores']);
+Route::get('/psicologos', [UserController::class, 'psicologos']);
+Route::post('/cambiar-password', [UserController::class, 'cambiarPassword']);
